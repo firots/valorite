e0d825b361c3998a4741d99d3372d0caeb8882ec
Fırat Özsarıkamış <firat.ozkms@gmail.com>
2024-04-09 21:56:36 +0300

    Update constants to support macos

--- src/constants.rs
+++ src/constants.rs
@@ -1,10 +1,25 @@
 pub const APP_NAME: &str = "Valor Launcher";
+
+#[cfg(target_os = "windows")]
 pub const CUO_FILES_URL: &str  = "http://valor.gen.tr/cuo_files_win";
+#[cfg(target_os = "macos")]
+pub const CUO_FILES_URL: &str  = "http://valor.gen.tr/cuo_files_mac";
+
 pub const UPDATE_FILE_NAME: &str  = "update.json";
 pub const COMPRESSION_EXTENSION: &str = ".zip";
+
+#[cfg(target_os = "windows")]
 pub const LAUNCHER_EXECUTABLE_PATH: &str = "/valorite.exe";
+#[cfg(target_os = "macos")]
+pub const LAUNCHER_EXECUTABLE_PATH: &str = "/valorite";
+
 pub const CLIENT_FOLDER_PATH: &str = "valor_cuo/";
+
+#[cfg(target_os = "windows")]
 pub const CLIENT_BINARY: &str = "cuo.exe";
+#[cfg(target_os = "macos")]
+pub const CLIENT_BINARY: &str = "cuo";
+
 pub const CHECKING_FOR_UPDATES: &str = "Guncellemeler denetleniyor...";
 pub const DOWNLOADING_FILE: &str = "Indiriliyor: ";
 pub const UPDATING_FILE: &str = "Guncelleniyor: ";
